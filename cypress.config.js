1a2b824ca8551e697d0da320ac477a525734a87d
Alexander Neufeldt <63143061+AlexanderNeufeldt@users.noreply.github.com>
2024-07-09 21:35:03 -0400

    Implemented Update function
    Massive overhaul to get google.update working. Had to change how we call sheets api.

--- cypress.config.js
+++ cypress.config.js
@@ -11,11 +11,43 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {    
+        
+        /*params contains the follwing object: 
+        {
+          spreadSheetID: the identifier for the spreadsheet to update
+          range: the sheets formula range ie. A1:B69 to update
+          values: array of arrays representing values for cells within the update range
+        }
+
+        */
+        async GoogleUpdate(params) {    //declares a task function named GoogleUpdate that we use to update a sheet
+          const auth = new google.auth.GoogleAuth({    //sets up a google authentication object for our account
+            keyFile: "CREDENTIALS.json",    // gives the auth object our credentials json for service account authentication
+            scopes: "https://www.googleapis.com/auth/spreadsheets",  // lets out update call have access to google sheets api
+          });
+
+          const sheets = google.sheets({    //make a variable that contains a sheets api client
+              version: 'v4',  // every example ever has this set to this
+          }); 
+          google.options({auth})  // set our auth object as the global auth option, this applies to all google apis, just a settings thing
+          const res = await sheets.spreadsheets.values.update({  // this is the function that actually does the updating, spec passes in relevant info
+            spreadsheetId: params.spreadSheetID,  // a variable that gets passed the spreadsheet ID when called in spec
+            range: params.range,  // a variable that gets the updated target range when called in spec
+            valueInputOption: "USER_ENTERED", // defines how google will interpret the passed data, options are Raw, user_entered, "date"
+            resource: {   // the body of the request to sheets, this is a object
+              values: params.values,  //
+            },
+          });
+
+          return res;  // returns the range results to const res
+        },
+
+
         async google(params) {    //declares a task function named google
           const sheets = google.sheets({    //make a variable that contains a sheets api client using API_KEY auth
               version: 'v4',
               auth: params.apiKey
-          });
+          }); 
 
           const res = await sheets.spreadsheets.values.get(   //gets the result of a values.get call and assigns those results to res
           //info on .get  https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
@@ -26,24 +58,8 @@ module.exports = defineConfig({
           );
           return res;  // returns the range results to const res
         },
-      })
-    
-      on('task', {    
-        async googleupdate(params) {    //declares a task function named googleupdate
-          const sheets = google.sheets({    //make a variable that contains a sheets api client using API_KEY auth
-              version: 'v4',
-              auth: params.apiKey
-          });
 
-          const res = await sheets.spreadsheets.values.update(   //API call that lets you write a single range
-          //info on .update  https://developers.google.com/sheets/api/guides/values
-              {
-                  spreadsheetId: params.spreadSheetID,  //put in spreadsheet ID aka what you're targeting
-                  range: params.range,  //lets the range be customized when the function is called
-              }
-          );
-          return res;  // returns the range results to const res
-        },
+        
       })
     
 
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,6 +11,7 @@ describe('template spec', () => {
     const spreadSheetID = Cypress.env('SpreadSheetID');  //sets API_KEY to the hidden APIKey & spreadSheetID to hidden SpreadSheetID in cypress.env.json
     const range = "MainSheet!A2:C22";  //sets the range param to mostly the whole sheet
 
+
     cy.task('google', {apiKey: apiKey, spreadSheetID: spreadSheetID, range: range}, {log: false}).then((res) => {  /*executes the google task function in 
     cypress.config.js. 
     cy.task('google').then((res) => {  /*executes the google task function in cypress.config.js. 
@@ -30,39 +31,49 @@ describe('template spec', () => {
 
   })
 
-  it.only ('FIND & SEARCH THE 13TH ENTRY', () => {
+  it ('FIND & SEARCH THE 13TH ENTRY', () => {
     const apiKey = Cypress.env('SheetAPIKey');  //sets API_KEY to the hidden APIKey in cyrpess.env.json
     const spreadSheetID = Cypress.env('SpreadSheetID');  //sets API_KEY to the hidden APIKey & spreadSheetID to hidden SpreadSheetID in cypress.env.json
-    const range = "MainSheet!B13";  //sets the range param to a specific card
-
+    const range = "MainSheet!B11:B26";  //sets the range param to a specific card
+    var values = [];  //
+    
     cy.task('google', {apiKey: apiKey, spreadSheetID: spreadSheetID, range: range }, {log: false}).then((res) => {  
-
-        const TargetCard = 	res.data.values;  //sets TargetCard to the function output, aka B13
-
-        cy.visit('https://store.401games.ca/') // visit 401games
-        cy.wait(2000)
+      cy.visit('https://store.401games.ca/') // visit 401games
+      cy.wait(2000)
+      
+      for ( const TargetCard of res.data.values){  
+        
         cy.log(`${TargetCard}`);  //prints card name in console
 
         cy.get('#isp_main_input') //get the search field 
+          .clear()
           .type(String(TargetCard))  //types the card we're looking for
         cy.get('#isp_main_search_button')  //find and click the search box
           .click()
         
         cy.url().then(($url) => {  //have to use this because you can't assign any cy.commands to vars
-          const URL401games = $url;  //asigns current URL to const var
-          cy.log(`${URL401games}`);  //prints card name in console
+          values.push([$url]);  //adds the urls to the let values array delcared above
+          cy.log(`${values}`);  //prints card name in console
           cy.log(`${TargetCard}`);  //prints card name in console
 
         })
-
+        cy.wait(10000)
+      }
 
     })
 
+    //write URL to sheet
+    //const apiKey = Cypress.env('SheetAPIKey');  //sets API_KEY to the hidden APIKey in cyrpess.env.json
+   // const spreadSheetID = Cypress.env('SpreadSheetID');  //sets API_KEY to the hidden APIKey & spreadSheetID to hidden SpreadSheetID in cypress.env.json
+    const updaterange = "MainSheet!C11:C26";  //sets the range param to a specific card
+    cy.task('GoogleUpdate', { spreadSheetID: spreadSheetID, range: updaterange, values: values }, {log: false}).then((res) => {  
+
+      cy.log('HHHEEELLLOOO WORLDDDDDDDDDDDDDDDDD')
+  
+      })
+
+
   })
-
-
-
-
 
   
 })
